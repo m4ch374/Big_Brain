66856ac3e53cfd6c9af6aef24c6d58874dfe0eed
Henry Wan <z5338828@ad.unsw.edu.au>
2023-04-06 00:34:50 +1000

    Msic: change browser router location

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import ProtectedRoutes from './components/ProtectedRoutes';
 import Dashboard from './pages/Dashboard';
 import EditQuestion from './pages/EditQuestion';
@@ -10,19 +10,17 @@ import Register from './pages/Register';
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<ProtectedRoutes />}>
-          <Route path='/' element={<Dashboard />} />
-          <Route path='/dashboard' element={<Dashboard />} />
-          <Route path='/quiz/:quizId' element={<EditQuiz />} />
-          <Route path='/question/:quizId/:questionId' element={<EditQuestion />} />
-        </Route>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path='/' element={<ProtectedRoutes />}>
+        <Route path='/' element={<Dashboard />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/quiz/:quizId' element={<EditQuiz />} />
+        <Route path='/question/:quizId/:questionId' element={<EditQuestion />} />
+      </Route>
+      <Route path='/login' element={<Login />} />
+      <Route path='/register' element={<Register />} />
+      <Route path='*' element={<NotFound />} />
+    </Routes>
   );
 }
 
